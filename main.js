d71473f5686e8bc90f0b4c1f071cbab3856c6652
Dmitry Scriptin <scrptn@gmail.com>
2014-09-29 21:49:04 +0400

    Do not refresh editor every time, check if something changed

--- main.js
+++ main.js
@@ -2,27 +2,39 @@ define(function (require, exports, module) {
   "use strict";
 
   // Indent soft wraps, but only when line is indented with spaces
-  function handleEditor(editor) {
+  function handleEditor(editor, forceRefresh) {
     if (!editor) return;
+    var changedSomething = false;
     editor._codeMirror.on("renderLine", function (cm, line, elt) {
       var firstNonSpace = line.text.search(/[^\s\t]/);
       var tabsCount = line.text.substr(0, firstNonSpace).replace(/[^\t]/g, "").length;
       var off = firstNonSpace * cm.defaultCharWidth() + tabsCount * cm.getOption("tabSize");
       if (tabsCount === 0) {
-        elt.style.textIndent = "-" + off + "px";
-        elt.style.paddingLeft = off + "px";
+        var compStyle = window.getComputedStyle(elt, null),
+            textIndent = "-" + off + "px",
+            paddingLeft = off + "px";
+        if (textIndent != compStyle["text-indent"]) {
+          elt.style.textIndent = textIndent;
+          changedSomething = true;
+        }
+        if (paddingLeft != compStyle["padding-left"]) {
+          elt.style.paddingLeft = paddingLeft;
+          changedSomething = true;
+        }
       }
     });
-    editor.refresh();
+    if (changedSomething || forceRefresh) {
+      editor.refresh();
+    }
   }
 
   var AppInit = brackets.getModule('utils/AppInit'),
       EditorManager = brackets.getModule("editor/EditorManager");
 
   AppInit.appReady(function () {
-    handleEditor(EditorManager.getCurrentFullEditor());
+    handleEditor(EditorManager.getCurrentFullEditor(), true);
     $(EditorManager).on('activeEditorChange', function (event, focusedEditor, lostEditor) {
-      handleEditor(focusedEditor);
+      handleEditor(focusedEditor, false);
     });
   });
 });
